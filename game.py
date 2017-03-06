a3d753d66447bfbcf3ed4e74a9557421e51fc7d2
Paweł Fertyk <pfertyk@openmailbox.org>
2017-03-06 19:41:52 +0100

    Display asteroids

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ pygame.init()
 SCREEN = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Asteroids')
 IMG_EARTH = pygame.image.load('earth.png').convert()
+IMG_ASTEROID_BIG = pygame.image.load('asteroid-big.png').convert_alpha()
 CLOCK = pygame.time.Clock()
 
 # GAME CLASSES AND METHODS
@@ -23,5 +24,7 @@ while not done:
 
     # DRAWING
     SCREEN.blit(IMG_EARTH, (0, 0))
+    SCREEN.blit(IMG_ASTEROID_BIG, (100, 100))
+    SCREEN.blit(IMG_ASTEROID_BIG, (300, 400))
     pygame.display.flip()
     CLOCK.tick(60)
