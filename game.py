84d543438f63292b91e7ff3d1b8139bc96a21ac9
Paweł Fertyk <pfertyk@openmailbox.org>
2017-03-13 19:12:17 +0100

    Use keys to rotate the starship

--- game.py
+++ game.py
@@ -31,8 +31,25 @@ class Asteroid(PyladiesGameObject):
 class Starship(PyladiesGameObject):
     image = IMG_STARSHIP
 
+    def __init__(self, pos):
+        super().__init__(pos, 20, 0.0, 0)
+        self.angle = 0
+
+    def _rotate(self, clockwise=True):
+        if clockwise:
+            delta = 1
+        else:
+            delta = -1
+        self.angle += delta
+
+    def rotate_clockwise(self):
+        self._rotate(True)
+
+    def rotate_counterclockwise(self):
+        self._rotate(False)
+
     def draw(self, surface):
-        rotated_surf = pygame.transform.rotozoom(self.image, -3000, 1.0)
+        rotated_surf = pygame.transform.rotozoom(self.image, -self.angle, 1.0)
         blit_pos = [coord - self.radius for coord in self.pos]
         surface.blit(rotated_surf, blit_pos)
 
@@ -46,13 +63,18 @@ for _ in range(6):
         get_random_pos(SCREEN.get_width(), SCREEN.get_height()), 50)
     )
 
-starship = Starship((400, 300), 20, 0, 0)
+starship = Starship((400, 300))
 
 while not done:
     # EVENTS
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+
+    if pygame.key.get_pressed()[pygame.K_RIGHT]:
+        starship.rotate_clockwise()
+    elif pygame.key.get_pressed()[pygame.K_LEFT]:
+        starship.rotate_counterclockwise()
 
     # LOGIC
     for asteroid in asteroids:
